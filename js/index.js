49608081da9aee0f3fe05735a27b2f19aa5d4ba2
KamajorQA <KamajorQA@gmail.com>
2023-07-28 22:35:02 +0300

    Инициализирован свайпер в js-коде. Обновлена стилизация кнопок и пагинации.

--- js/index.js
+++ js/index.js
@@ -2,3 +2,25 @@ let currentTime = function () {
   console.log(new Date().getHours() + ':' + new Date().getMinutes());
 };
 currentTime();
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});
